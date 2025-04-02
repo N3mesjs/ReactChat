1f68d186374def16e00dc05753f84a7a11112c92
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-04-02 10:58:31 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useEffect } from 'react';
+
 import SignInPage from '../components/signInPage';
 
 export default function Page() {
+
+    useEffect(() => {
+        fetch('/api/')
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    }, []);
     return (
         <>
             <SignInPage />
--- components/signInPage.tsx
+++ components/signInPage.tsx
@@ -31,19 +31,19 @@ export default function SignInPage() {
     }
 
     return (
-        <main className="bg-gray-900 w-screen h-screen flex flex-col text-white justify-center items-center gap-[3em]">
+        <main className="main">
             <h1 className='text-[2em] font-bold'>SignIn to Nymes's Chat App</h1>
-            <form className="flex flex-col bg-black p-7 rounded-3xl gap-6" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor="username">Username</label>
-                    <input type="text" placeholder="Username" className="rounded-[3px] p-3 border-[1px] focus:outline-none" id="username" onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" placeholder="Username" className="inpt" id="username" onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div className='flex flex-col gap-1'>
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Password" className="rounded-[3px] p-3 border-[1px] focus:outline-none" id="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" placeholder="Password" className="inpt" id="password" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
 
-                <button type="submit" className="bg-blue-500 rounded-3xl p-3 hover:bg-blue-700">SignIn</button>
+                <button type="submit" className="send-btn">SignIn</button>
             </form>
         </main>
     )
